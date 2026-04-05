be9aaba5cfecda3b8bac4b787ac6254b996169d8
Anshika Chauhan <chauhanshikaa1@gmail.com>
2026-04-06 02:18:08 +0530

    Update main.py

--- numpy_foundation/numerical_computing_engine/ src/main.py
+++ numpy_foundation/numerical_computing_engine/ src/main.py
@@ -1,8 +1,14 @@
 from ingestion import load_sensor_data
+from processing import compute_mean, compute_std
 
 def main():
     data = load_sensor_data("../data/sensor_data.csv")
-    print("Raw Sensor Data:", data)
+
+    mean = compute_mean(data)
+    std = compute_std(data)
+
+    print("Mean:", mean)
+    print("Standard Deviation:", std)
 
 if __name__ == "__main__":
     main()
